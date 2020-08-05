818c101f7303e2bcdc175c30e698634c03a36924
Fery J <ferydjzz@gmail.com>
2020-08-05 15:43:33 +0700

    fix typo fourty to forty

--- lib/say_number/languages/english.rb
+++ lib/say_number/languages/english.rb
@@ -2,7 +2,7 @@ class English
   def self.initialize_sayer
     @satuan_en = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     @teen = {10=>"ten", 11=>"eleven", 12=>"twelve", 13=>"thirteen", 14=>"fourteen", 15=>"fifteen", 16=>"sixteen", 17=>"seventeen", 18=>"eighteen", 19=>"nineteen"}
-    @anomalies = {20=>"twenty", 30=>"thirty", 40=>"fourty", 50=>"fifty", 60=>"sixty", 70=>"seventy", 80=>"eighty", 90=>"ninety", 100=>"hundred", 1000=>"thousand", 1000000=>"million", 1000000000=>"billion", 1000000000000=>"trillion", 1000000000000000=>"quadrillion"}
+    @anomalies = {20=>"twenty", 30=>"thirty", 40=>"forty", 50=>"fifty", 60=>"sixty", 70=>"seventy", 80=>"eighty", 90=>"ninety", 100=>"hundred", 1000=>"thousand", 1000000=>"million", 1000000000=>"billion", 1000000000000=>"trillion", 1000000000000000=>"quadrillion"}
   end
 
   def self.say_koma(koma)
